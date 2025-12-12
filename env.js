cab582c47b47f34452495050f166ad38f149cc61
Hima-Kumi <125667773+Hima-Kumi@users.noreply.github.com>
2025-12-12 21:56:22 +0900

    Add files via upload

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 // Google Apps ScriptのデプロイURLをここに設定してください。
 // このURLは、Google Apps Scriptエディタで「デプロイ」->「新しいデプロイ」を実行した際に発行されるURLです。
 // URLの末尾が必ず「/exec」で終わっていることを確認してください。
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyyKnKVitI2igF3mnCQDSj3fsdLsDzc5iXTmiOiztv37xf1SM1dxKat3RZjBW_n2Wk/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzOslXJlWCD4SZ_MTx1ITcgiNug16ObA60JhRcHs7bUv_Fn_5uYg9GlXY2ZK8HwnYs/exec";
 
 // 各HTMLファイル (app.html, admin.html) は、この window.API_URL を参照します。
 window.API_URL = GAS_URL;
